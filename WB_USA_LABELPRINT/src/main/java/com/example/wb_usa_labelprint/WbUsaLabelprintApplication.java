0447e12ffe9765efd5930d1aa570acf225facafc
IT5\HanmacSystem <choidh0930@naver.com>
2026-05-20 12:59:19 +0900

    Initialize

---
+++ WB_USA_LABELPRINT/src/main/java/com/example/wb_usa_labelprint/WbUsaLabelprintApplication.java
@@ -0,0 +1,13 @@
+package com.example.wb_usa_labelprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbUsaLabelprintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WbUsaLabelprintApplication.class, args);
+    }
+
+}
